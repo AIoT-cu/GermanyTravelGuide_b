d3bb9206d497c4381fa69fac927ee13f67c6da0d
Strely02 <sandrapm.9811@gmail.com>
2024-01-23 13:54:02 -0500

    feat: import places routes

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const port = 3000;
 // Middleware to parse JSON
 app.use(express.json());
 
+// Import the routes
+const placesRoutes = require('./routes/placesRoutes');
+
+// Associate the routes with the app
+app.use('/', placesRoutes);
 
 // Start the server
 app.listen(port, () => {
