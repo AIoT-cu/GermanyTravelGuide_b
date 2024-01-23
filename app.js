dd2d23e4d6a7cbcf1c774fb259332fb0777f4a71
Strely02 <sandrapm.9811@gmail.com>
2024-01-23 13:53:17 -0500

    refactor: adjust code format

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!')
-})
-app.listen(3000, () => {
-    console.log('Servidor iniciado en el puerto 3000')
-})
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`The application is listening at http://localhost:${port}`);
+});
