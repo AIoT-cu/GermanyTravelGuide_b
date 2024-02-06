158c174597dd339980fad6b102ca826d1165fe13
Luis Quintero <laqdominguez@gmail.com>
2024-02-06 14:23:53 -0500

    feat: add the helper sequelize to coonect the orm to the database

---
+++ helpers/sequelize.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+// Initialize DB connection
+const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
+    host: `${process.env.DB_HOST}`,
+    dialect: `${process.env.DB_DIALECT}`, /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    logging: console.log,
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     //rejectUnauthorized: false // This is optional depending on your configuration
+    //   }
+    // }
+  });
+  // test db connection
+  // Use the .authenticate() method to test the connection
+  sequelize.authenticate()
+    .then(() => {
+      console.log('Connection to DB successful');
+    })
+    .catch(err => {
+      console.log('Connection to DB failed');
+      console.log(err);
+    });
+
+    module.exports = sequelize
