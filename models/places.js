6e8788807ecc668dee3f5da9c8ff36fab5e67f52
Strely02 <sandrapm.9811@gmail.com>
2024-02-06 15:47:18 -0500

    fix: solve problem with places model extension

--- models/places.js
+++ models/places.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/sequelize");
+const sequelize = require("../helpers/sequelize");
 
 const Places = sequelize.define(
     "places",
