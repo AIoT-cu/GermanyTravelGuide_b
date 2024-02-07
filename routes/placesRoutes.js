0f6f4493605148e446373b8fe96e1b6e7fccf0f0
Strely02 <sandrapm.9811@gmail.com>
2024-02-07 17:25:28 -0500

    feat: add routes to create and delete places

--- routes/placesRoutes.js
+++ routes/placesRoutes.js
@@ -7,5 +7,39 @@ const placesController = require('../controllers/placesController');
 // Define routes using the controllers
 router.get('/places', placesController.getAllPlaces);
 router.get('/places/:id', placesController.getPlaceDetailsById);
+router.post('/places/create', async (req, res) => {
+    const place_name = req.body.place_name;
+    const state_name = req.body.state_name;
+    const place_description = req.body.place_description;
+    const place_location = req.body.place_location;
+    const place_image_url = req.body.place_image_url;
+
+    const place = await placesController.createPlace(
+        place_name,
+        state_name,
+        place_description,
+        place_location,
+        place_image_url)
+    
+    if(place.error){
+        res.status(500).json({message: place.error});
+    }else{
+        res.json(place);
+    }
+});
+
+router.delete('/places/:id', async (req, res) => {
+    const placeID = req.params.id;
+
+    const deleted = await placesController.deletePlace(placeID)
+    
+    if(deleted.error){
+        res.status(500).json({message: deleted.error});
+    }else if(deleted === 0){
+        res.status(500).json({message: `Place with id ${placeID} does not exist`});
+    }{
+        res.json({placeID, deleted});
+    }
+});
 
 module.exports = router;
