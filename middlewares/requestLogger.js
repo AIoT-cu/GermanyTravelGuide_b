0c17864e35122466f84f7c7841104aa87e5f2358
Strely02 <sandrapm.9811@gmail.com>
2024-02-13 09:40:48 -0500

    feat: add request logger middlewares

---
+++ middlewares/requestLogger.js
@@ -0,0 +1,8 @@
+const logger = require("../logger/logger");
+
+const requestLogger = (req, res, next) => {
+  logger.info(`${req.method} ${req.originalUrl} from ${req.ip}`);
+  next();
+};
+
+module.exports = requestLogger;
