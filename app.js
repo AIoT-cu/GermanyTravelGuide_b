2d5bc7b50e5f3fe20112b1b0a78924af522743b2
Strely02 <sandrapm.9811@gmail.com>
2024-04-10 13:01:49 -0400

    feat: add some changes

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ const logger = require("./logger/logger");
 const requestLogger = require("./middlewares/requestLogger");
 const errorLogger = require("./middlewares/errorLogger");
 const errorHandler = require("./middlewares/errorHandler");
-const authenticate = require('./middlewares/authenticate');
+// const authenticate = require('./middlewares/authenticate');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const AppError = require("./errors/AppError");
 
 const app = express();
+
+// Confugure Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Germany Travel Blog',
+      version: '1.0.0',
+      description: 'API Germany Travel Blog',
+    },
+  },
+  apis: ['./routes/*.js', './models/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Configure CORS
 app.use(
@@ -54,12 +73,14 @@ const placesRoutes = require('./routes/placesRoutes');
 const statesRoutes = require('./routes/statesRoutes');
 const categoriesRoutes = require('./routes/categoriesRoutes');
 const usersRoutes = require('./routes/users');
+const imagesRoutes = require('./routes/images');
 
 // Associate the routes with the app
 app.use('/', placesRoutes);
 app.use('/', statesRoutes);
 app.use('/', categoriesRoutes);
 app.use('/', usersRoutes);
+app.use('/', imagesRoutes);
 
 // Check if request to a non existing route
 app.all("*", (req, res, next) => {
@@ -71,7 +92,7 @@ app.all("*", (req, res, next) => {
   next(err);
 });
 
-// error handling
+// Error handling
 app.use(errorHandler);
 
 // Start the server
